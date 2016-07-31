8c88b9b82d5cc51719e6f968de16a8bb7003285d
zff <zhuceidzff@1663.com>
2016-07-31 16:17:01 +0800

    使用AsyncTask类实现子线程切换到主线程

--- app/src/main/java/com/example/lsx/threaddemo/MainActivity.java
+++ app/src/main/java/com/example/lsx/threaddemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lsx.threaddemo;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
         mLoadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadImage();
+                //方式一
+                // loadImage();
+                //方式二
+                new LoadImageTask().execute();
             }
         });
     }
 
-    private void loadImage() {
+    /*private void loadImage() {
 
         new Thread(new Runnable() {
             @Override
@@ -50,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 mImageView.post(new Runnable() {
+                    //post,把runable取出来，推到主线程的message queue中，等待Looper发现并执行其中的run()
                     @Override
                     public void run() {
                         mImageView.setImageBitmap(bitmap);
@@ -58,5 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }).start();
+    }*/
+    //使用AsyncTask类的方法实现子线程切换到主线程
+    class LoadImageTask extends AsyncTask<Void,Void,Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            mImageView.setImageBitmap(bitmap);
+        }
     }
 }
